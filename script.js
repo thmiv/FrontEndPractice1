4c413284f583a55c8944feb4342218687f174b1d
thmiv <thmiv4@gmail.com>
2019-02-11 10:32:33 -0600

    FE practice from skillshare

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('hey there!')
+//console.log('testing aknflanfa');
+
+function showPopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.add('show');
+}
+
+function hidePopup () {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('show');
+}
